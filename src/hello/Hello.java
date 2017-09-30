bdfdf06d94c8a2aa3efe9c1bd556af1a9bdbe8d2
Joshmejor <joshmejorado@gmail.com>
2017-09-30 15:14:06 -0700

    project setup, first commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Hello 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World, and welcome to CS 3337 Software Engineering");
+
+	}
+	
+}	
